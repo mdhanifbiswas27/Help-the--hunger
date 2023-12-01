0ca6537c590623288abf110f73b0493ddfd70b2b
mdhanifbiswas27 <mdhanifbiswas27@gmail.com>
2023-12-02 04:09:24 +0600

    banner and Our impact section related some issues has solved

--- src/Routes/Pages/Home/Banner.jsx
+++ src/Routes/Pages/Home/Banner.jsx
@@ -2,10 +2,12 @@ import banner from'../../../assets/donation-final.jpg'
 
 const Banner = () => {
     return (
-        <div className='relative'>
-            <div><img src={banner} alt="" /></div>
-            <div className='absolute top-0 '>
-                    <div>hi</div>
+        <div className='relative '>
+            <div><img className='h-[600px] w-full' src={banner} alt="" /></div>
+            <div className='bg-opacity-10 absolute top-0 bg-gradient-to-r from-[#BB272E] to-[rgba(21, 21, 21, 0)] h-full w-full'>
+                    <div className=' translate-y-[160px]'>
+                         <h1 className='text-white font-extrabold  text-[100px] leading-[100px]'>Share <br /> Your Extra Food <br /> To Hunger</h1>
+                    </div>
             </div>
         </div>
     );
