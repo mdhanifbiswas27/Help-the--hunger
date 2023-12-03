cce8a20e94f61c2f9ac60f24ec1a65e79dcd8dea
mdhanifbiswas27 <mdhanifbiswas27@gmail.com>
2023-12-03 14:07:41 +0600

    error page photo and some text added

--- src/Routes/Pages/Error/Error.jsx
+++ src/Routes/Pages/Error/Error.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
-
+import photoo from '../../../assets/Simple-Pure-CSS3-404-Error-Page.gif'
 
 const Error = () => {
     return (
-        <div className="max-w-screen-xl mx-auto flex justify-center items-center min-h-screen">
-            <div>
+        <div className="relative max-w-screen-xl mx-auto flex justify-center items-center max-h-screen">
+            
+            <div className="">
+                <img src={photoo} alt="" />
+            </div>
+            <div className="absolute bottom-10">
                 <h1 className="text-[30px] font-medium text-red-500">Something wrong here....</h1>
                 <Link to='/'><button className="px-4 py-2 border-2 border-red-500">Home</button></Link>
             </div>
